16ca22dfe9f7f4ea3d30014840548774ab21c0ba
Shubhammatole16 <shubhammatole1@gmail.com>
2023-07-06 20:51:28 +0530

    Update javacript.js

--- javacript.js
+++ javacript.js
@@ -1,35 +1,5 @@
-
-
-//SEND AN EMAIL USING EAMIL JS
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var message = document.getElementById('message').value;
-    var formData = {
-        name: name,
-        email: email,
-        phone: phone,
-        message: message
-    };
-emailjs.send('service_sm1631', 'template_a1q4ikp', formData)   
-    .then(function(response) {
-        console.log('Email sent successfully!', response);
-        displayPopup('Email Sent Successfully!\n Thank You For Contacing Me.\n I will get back to you soon.');
-        document.getElementById('contactForm').reset();
-        setTimeout(function() {
-            window.location.href = 'index.html';
-        }, 3000);
-    })
-    .catch(function(error) {
-        console.error('Email sending failed:', error);
-        displayPopup('Email Sending Failed. Please Try Again Later.');
-    });
-});
-
-
 //CUSTON POPUP MESSAGE BOX
+
 function displayPopup(message) {
     var popupContainer = document.getElementById('popupContainer');
     var popupMessage = document.getElementById('popupMessage');
@@ -43,7 +13,10 @@ function displayPopup(message) {
 
 
 
+
+
 // VALIDATION OF EMAIL AND MOBILE NUMBER
+
 function validateForm() {
   var email = document.getElementById("email").value;
   var mobile = document.getElementById("phone").value;
